dc57ba08d736f5a31c8e47189957dc48763ca9f3
matu184 <matu184@live.de>
2023-12-18 12:05:55 +0100

    Projektdateien hinzufügen.

---
+++ Leistung/Global.asax.cs
@@ -0,0 +1,92 @@
+﻿using Fleck;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Sockets;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Script.Serialization;
+using System.Threading;
+using System.EnterpriseServices;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace Leistung
+{
+    public class Global : System.Web.HttpApplication
+    {
+        private static List<IWebSocketConnection> allSockets = new List<IWebSocketConnection>();
+        private static WebSocketServer server = new WebSocketServer("ws://127.0.0.1:8081");
+        private static JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+        private static PerformanceCounter cpuCounter;
+        private static PerformanceCounter ramCounter;
+        private static PerformanceCounter diskCounter;
+        
+
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            cpuCounter = new PerformanceCounter("Prozessor", "Prozessorzeit (%)", "_Total");
+            ramCounter = new PerformanceCounter("Arbeitsspeicher", "Verfügbare MB");
+            diskCounter = new PerformanceCounter("LogicalDisk", "% Free Space", "_Total");
+
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    allSockets.Add(socket);
+                    Nachricht();
+
+                };
+                socket.OnClose = () =>
+                {
+                    allSockets.Remove(socket);
+                };
+
+                socket.OnMessage = message =>
+                {
+                 
+                    socket.Send(serializer.Serialize(Nachricht("Hallo")));
+                };
+
+            });
+            
+
+
+        }
+        protected static void Nachricht()
+        {
+            string cpuvalue = cpuCounter.NextValue().ToString();
+            string diskvalue = diskCounter.NextValue().ToString();
+            string ramvalue = ramCounter.NextValue().ToString();
+            
+            Nachricht zählerNachricht = new Nachricht { Cpu = cpuvalue, Disk = diskvalue, Mem = ramvalue };
+            foreach (IWebSocketConnection s in allSockets.ToList())
+            {
+                s.Send(serializer.Serialize(zählerNachricht));
+            }
+        } 
+        protected static Nachricht Nachricht(string x)
+        {
+            string cpuvalue = cpuCounter.NextValue().ToString();
+            string diskvalue = diskCounter.NextValue().ToString();
+            string ramvalue = ramCounter.NextValue().ToString();
+            
+            Nachricht zählerNachricht = new Nachricht { Cpu = cpuvalue, Disk = diskvalue, Mem = ramvalue };
+            return zählerNachricht;
+            
+        }
+
+
+    }
+    public class Nachricht
+    {
+        public string Cpu { get; set; }
+        public string Mem { get; set; }
+        public string Disk { get; set; }
+    }
+}
---
+++ Leistung/JavaScript.js
@@ -0,0 +1,68 @@
+﻿let socket;
+var timeout = setInterval(UpdateDaten, 5000);
+
+function UpdateDaten() {
+
+    socket.send("s");
+
+}
+window.onload = function () {
+
+    socket = new WebSocket('ws://127.0.0.1:8081');
+
+    function updateIntervall() {
+        var intervallValue = document.getElementById("intervallInput").value;
+        clearInterval(timeout);
+        timeout = setInterval(UpdateDaten, intervallValue*1000);
+        document.getElementById("intervallValue").innerText = intervallValue;
+    }
+
+
+    document.getElementById("intervallInput").oninput = updateIntervall;
+
+    socket.onopen = function () {
+        alert("Verbindung hergestellt")
+
+    }
+    socket.onmessage = NachrichtEmpfang;
+    function NachrichtEmpfang(event) {
+        let data = JSON.parse(event.data);
+
+        console.log(event.data);
+        let cpu = data.Cpu;
+        let ram = data.Mem;
+        let hdd = data.Disk;
+        document.getElementById("cpu").innerHTML = "CPU: " + cpu + "%";
+        document.getElementById("memory").innerHTML = "Memory: " + ram + "MB";
+        document.getElementById("hdd").innerHTML = "HDD: " + hdd + "%";
+        document.getElementById("pro1").value = parseFloat(cpu);
+        document.getElementById("mem").value = parseFloat(ram);
+        document.getElementById("hd").value = parseFloat(hdd);
+
+        drawChart(parseFloat(cpu), parseFloat(ram), parseFloat(hdd))
+
+    }
+}
+google.charts.load('current', { 'packages': ['gauge'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart(cpulabel, ramlabel, disklabel) {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Memory', (ramlabel / 32768 * 100)],
+        ['CPU', cpulabel],
+        ['Disk', disklabel]
+    ]);
+
+    var options = {
+        width: 400, height: 120,
+        redFrom: 90, redTo: 100,
+        yellowFrom: 75, yellowTo: 90,
+        minorTicks: 5
+    };
+
+    var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+}
